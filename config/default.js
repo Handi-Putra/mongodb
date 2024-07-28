76f946c60dbcd56eff2c662884bb443f8930c882
Handi-Putra <43770290+Handi-Putra@users.noreply.github.com>
2024-07-28 15:58:43 +0700

    login auth mongodb

---
+++ config/default.js
@@ -0,0 +1,5 @@
+module.exports = {
+    port: 8000,
+    dbURI: "mongodb+srv://handiputratjioe:N55gXItVZryAQydJ@cluster0.hugdi8x.mongodb.net/loginauth?retryWrites=true&w=majority&appName=Cluster0",
+    saltFactor: 12
+}
